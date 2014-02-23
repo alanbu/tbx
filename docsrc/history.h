f91657a74be595c6ea12f33c60f86f5e21f82859
alanb <alanbu@users.noreply.github.com>
2014-02-23 20:06:06 +0000

    Added TaskManager class

--- docsrc/history.h
+++ docsrc/history.h
@@ -7,6 +7,7 @@
  * - Added Font::read_details to get the details of a font
  * - Added Application::yield to allow multi-tasking in the middle of a long operation
  * - Fixed possible memory corruption when removing idle commands from within a running idle command
+ * - Added TaskManager class to interrogate running tasks
  *
  * <B>0.7.1 Alpha December 2013</B>
  * - Window::remove_gadget now removes all listeners for the gadget
