c04a134be4f41300f6151b0e34fa60c794f1d043
alanb@RISCID.ORG <alanb@RISCID.ORG@8792eff0-d747-44ec-a27e-2ad34fde4ef4>
2014-06-13 08:29:08 +0000

    Fixed bug plotting 16M colour sprites

--- trunk/docsrc/history.h
+++ trunk/docsrc/history.h
@@ -3,8 +3,8 @@
  *
  * <B>0.7.x Alpha ???</B>
  *
- * Added PThreadGuard class to disable threading around wimp poll and event processing when using pthreads to prevent crashes caused by interrupting event processing.
- * Added prepoll and post event listener to application (used by PThreadGuard)
+ * - Added PThreadGuard class to disable threading around wimp poll and event processing when using pthreads to prevent crashes caused by interrupting event processing.
+ * - Added prepoll and post event listener to application (used by PThreadGuard) * - Fixed plotting of 16M colour sprites to any other mode caused a crash due to a bug in the TranslationTable class.
  *
  * <B>0.7.2 Alpha March 2014 </B>
  * - uncaught_exception method added to application to allow user processing of this error condition
